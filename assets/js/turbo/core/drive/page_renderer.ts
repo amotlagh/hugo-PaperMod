15787f21680971e38216c24b0fca0d586057cf54
Ali Motlagh <shahcheraghi.ali@gmail.com>
2023-01-26 16:16:07 +0330

    add turbolink and instantPage

---
+++ assets/js/turbo/core/drive/page_renderer.ts
@@ -0,0 +1,146 @@
+import { Renderer } from "../renderer"
+import { PageSnapshot } from "./page_snapshot"
+import { ReloadReason } from "../native/browser_adapter"
+import { activateScriptElement, waitForLoad } from "../../util"
+
+export class PageRenderer extends Renderer<HTMLBodyElement, PageSnapshot> {
+  static renderElement(currentElement: HTMLBodyElement, newElement: HTMLBodyElement) {
+    if (document.body && newElement instanceof HTMLBodyElement) {
+      document.body.replaceWith(newElement)
+    } else {
+      document.documentElement.appendChild(newElement)
+    }
+  }
+
+  get shouldRender() {
+    return this.newSnapshot.isVisitable && this.trackedElementsAreIdentical
+  }
+
+  get reloadReason(): ReloadReason {
+    if (!this.newSnapshot.isVisitable) {
+      return {
+        reason: "turbo_visit_control_is_reload",
+      }
+    }
+
+    if (!this.trackedElementsAreIdentical) {
+      return {
+        reason: "tracked_element_mismatch",
+      }
+    }
+  }
+
+  async prepareToRender() {
+    await this.mergeHead()
+  }
+
+  async render() {
+    if (this.willRender) {
+      this.replaceBody()
+    }
+  }
+
+  finishRendering() {
+    super.finishRendering()
+    if (!this.isPreview) {
+      this.focusFirstAutofocusableElement()
+    }
+  }
+
+  get currentHeadSnapshot() {
+    return this.currentSnapshot.headSnapshot
+  }
+
+  get newHeadSnapshot() {
+    return this.newSnapshot.headSnapshot
+  }
+
+  get newElement() {
+    return this.newSnapshot.element
+  }
+
+  async mergeHead() {
+    const newStylesheetElements = this.copyNewHeadStylesheetElements()
+    this.copyNewHeadScriptElements()
+    this.removeCurrentHeadProvisionalElements()
+    this.copyNewHeadProvisionalElements()
+    await newStylesheetElements
+  }
+
+  replaceBody() {
+    this.preservingPermanentElements(() => {
+      this.activateNewBody()
+      this.assignNewBody()
+    })
+  }
+
+  get trackedElementsAreIdentical() {
+    return this.currentHeadSnapshot.trackedElementSignature == this.newHeadSnapshot.trackedElementSignature
+  }
+
+  async copyNewHeadStylesheetElements() {
+    const loadingElements = []
+
+    for (const element of this.newHeadStylesheetElements) {
+      loadingElements.push(waitForLoad(element as HTMLLinkElement))
+
+      document.head.appendChild(element)
+    }
+
+    await Promise.all(loadingElements)
+  }
+
+  copyNewHeadScriptElements() {
+    for (const element of this.newHeadScriptElements) {
+      document.head.appendChild(activateScriptElement(element))
+    }
+  }
+
+  removeCurrentHeadProvisionalElements() {
+    for (const element of this.currentHeadProvisionalElements) {
+      document.head.removeChild(element)
+    }
+  }
+
+  copyNewHeadProvisionalElements() {
+    for (const element of this.newHeadProvisionalElements) {
+      document.head.appendChild(element)
+    }
+  }
+
+  activateNewBody() {
+    document.adoptNode(this.newElement)
+    this.activateNewBodyScriptElements()
+  }
+
+  activateNewBodyScriptElements() {
+    for (const inertScriptElement of this.newBodyScriptElements) {
+      const activatedScriptElement = activateScriptElement(inertScriptElement)
+      inertScriptElement.replaceWith(activatedScriptElement)
+    }
+  }
+
+  assignNewBody() {
+    this.renderElement(this.currentElement, this.newElement)
+  }
+
+  get newHeadStylesheetElements() {
+    return this.newHeadSnapshot.getStylesheetElementsNotInSnapshot(this.currentHeadSnapshot)
+  }
+
+  get newHeadScriptElements() {
+    return this.newHeadSnapshot.getScriptElementsNotInSnapshot(this.currentHeadSnapshot)
+  }
+
+  get currentHeadProvisionalElements() {
+    return this.currentHeadSnapshot.provisionalElements
+  }
+
+  get newHeadProvisionalElements() {
+    return this.newHeadSnapshot.provisionalElements
+  }
+
+  get newBodyScriptElements() {
+    return this.newElement.querySelectorAll("script")
+  }
+}
---
+++ assets/js/turbo/tests/fixtures/test.js
@@ -0,0 +1,68 @@
+(function(eventNames) {
+  function serializeToChannel(object, visited = new Set()) {
+    const returned = {}
+
+    for (const key in object) {
+      const value = object[key]
+
+      if (value instanceof URL) {
+        returned[key] = value.toJSON()
+      } else if (value instanceof Element) {
+        returned[key] = value.outerHTML
+      } else if (typeof value == "object") {
+        if (visited.has(value))  {
+          returned[key] = "skipped to prevent infinitely recursing"
+        } else {
+          visited.add(value)
+
+          returned[key] = serializeToChannel(value, visited)
+        }
+      } else {
+        returned[key] = value
+      }
+    }
+
+    return returned
+  }
+
+  window.eventLogs = []
+
+  for (var i = 0; i < eventNames.length; i++) {
+    var eventName = eventNames[i]
+    addEventListener(eventName, eventListener, false)
+  }
+
+  function eventListener(event) {
+    const skipped = document.documentElement.getAttribute("data-skip-event-details") || ""
+
+    eventLogs.push([event.type, serializeToChannel(skipped.includes(event.type) ? {} : event.detail), event.target.id])
+  }
+  window.mutationLogs = []
+
+   new MutationObserver((mutations) => {
+     for (const { attributeName, target } of mutations.filter(({ type }) => type == "attributes")) {
+       if (target instanceof Element) {
+         mutationLogs.push([attributeName, target.id, target.getAttribute(attributeName)])
+       }
+     }
+   }).observe(document, { subtree: true, childList: true, attributes: true })
+})([
+  "turbo:click",
+  "turbo:before-stream-render",
+  "turbo:before-cache",
+  "turbo:before-render",
+  "turbo:before-visit",
+  "turbo:load",
+  "turbo:render",
+  "turbo:before-fetch-request",
+  "turbo:submit-start",
+  "turbo:submit-end",
+  "turbo:before-fetch-response",
+  "turbo:visit",
+  "turbo:before-frame-render",
+  "turbo:fetch-request-error",
+  "turbo:frame-load",
+  "turbo:frame-render",
+  "turbo:frame-missing",
+  "turbo:reload"
+])
